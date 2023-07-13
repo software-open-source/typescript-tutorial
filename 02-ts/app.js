d9e6e4e58704136293c0193d510e4652ad537d96
Luong Vu <hide.dev.2022@gmail.com>
2023-07-13 08:18:45 +0700

    typescript-tutorial

---
+++ 02-ts/app.js
@@ -0,0 +1,8 @@
+{
+    let box = void 0;
+    console.log(typeof (box)); // undefined
+    box = "Hello";
+    console.log(typeof (box)); // string
+    box = 100;
+    console.log(typeof (box)); // number
+}
---
+++ 02-ts/app.ts
@@ -0,0 +1,12 @@
+{
+
+    let box;
+    console.log(typeof (box)); // undefined
+
+    box = "Hello";
+    console.log(typeof (box)); // string
+
+    box = 100;
+    console.log(typeof (box)); // number
+
+}
